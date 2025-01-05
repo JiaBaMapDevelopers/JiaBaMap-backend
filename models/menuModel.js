d7afae5c02317df033796c1f2fbcfdf70a6faadf
julie <juliewah8785@gmail.com>
2025-01-06 00:01:24 +0800

    fix: 修改菜單資料表欄位

--- models/menuModel.js
+++ models/menuModel.js
@@ -1,43 +1,49 @@
 const mongoose = require("mongoose");
-const Store = require("../models/storeModel");
 
 const menuSchema = new mongoose.Schema({
   storeId: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: "Store", // 關聯到餐廳
+    ref: "Store",
     required: true,
   },
-  singleDishId: {
-    type: String, //單一餐點id
-    required: true,
+  itemId: {
+    type: Number,
+    unique: true,
   },
   name: {
     type: String,
-    required: true,
-    trim: true,
+    required: [true, "名稱為必填項目"],
+    maxlength: 100,
   },
   price: {
     type: Number,
     required: true,
-  },
-  description: {
-    type: String, //餐點描述
-    trim: true,
+    min: 0,
   },
   category: {
-    type: String, // 如「主餐」、「飲料」等
-    trim: true,
-  },
-  placeId: {
-    //Google Place ID
     type: String,
-    required: true,
-    unique: true,
-    trim: true,
+    maxlength: 50,
+    required: [true, "分類為必填項目"],
   },
   isAvailable: {
-    type: Boolean, //是否可售
+    type: Boolean,
     default: true,
+  },
+  description: {
+    type: String,
+  },
+  placeId: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  imageUrl: {
+    type: String,
+    default: null,
   },
 });
 
