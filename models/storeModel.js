91626483ff6471415ca611903de5b03987e38a97
wanruliao <wan0917ru@gmail.com>
2025-01-07 15:46:13 +0800

    feat: 修改storeModel
    修改storeModel，將店家註冊資訊新增進去。

--- models/storeModel.js
+++ models/storeModel.js
@@ -2,28 +2,48 @@ const mongoose = require("mongoose");
 
 const storeSchema = new mongoose.Schema(
   {
-    name: {
+    username: {
       type: String,
-      required: true,
-      trim: true, //去除空格
-    },
-    address: {
-      type: String,
-      required: true,
       trim: true,
+    },
+    password: {
+      type: String,
+      trim: true,
+    },
+    storeName: {
+      type: String,
+    },
+    storeAddress: {
+      type: String,
+    },
+    storePhone: {
+      type: String,
+      trim: true,
+    },
+    storeIntro: {
+      type: String,
+    },
+    storeTaxId: {
+      type: String,
+    },
+    contactName: {
+      type: String,
+    },
+    contactEmail: {
+      type: String,
+    },
+    contactPhone: {
+      type: String,
+    },
+    placeId: {
+      type: String,
     },
     businessHours: {
       type: Map, //類似JS的字典
       of: String, //指定每個值的型別
-      required: true,
-    },
-    phone: {
-      type: String,
-      required: true,
-      trim: true,
     },
     image: {
-      type: String,
+      type: [String],
       default: [],
     },
     createAt: {
@@ -34,27 +54,8 @@ const storeSchema = new mongoose.Schema(
       type: Date,
       default: Date.now,
     },
-    taxId: {
-      //統一編號
-      type: String,
-      required: true,
-      trim: true,
-      unique: true,
-    },
     paymentOptions: {
       type: [String], //e.g. ["online", "cash"]
-      required: true,
-    },
-    description: {
-      type: String,
-      trim: true,
-    },
-    placeId: {
-      //Google Place ID
-      type: String,
-      required: true,
-      unique: true,
-      trim: true,
     },
   },
   {
